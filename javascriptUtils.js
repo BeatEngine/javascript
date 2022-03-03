6fb97b0f9548612c7483315f030f1f0c3f30ff52
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2022-03-03 14:25:28 +0100

    Update 1.0 executeForm takes care of selects.

--- javascriptUtils.js
+++ javascriptUtils.js
@@ -1,6 +1,3 @@
-/* This is useful for tomcat applications server.xy/applicationname/home */
-var applicationname = ".";
-
 /** Asynchrone Javascript Request - Aufruf der ServerMethode
  * @Param method anfordernde HTTP-Methode, wie GET und POST
  * @Param url wie "domain"/Pfad
@@ -13,8 +10,8 @@ function asyncRequest(method, url, body, onloadCallback, size = 0, contenttype =
 {
     var xhr = new XMLHttpRequest();
 
-    xhr.open(method,"/"+applicationname+url, true);
-    if(contenttype.length > 0)
+    xhr.open(method, "/" + applicationname + url, true);
+    if (contenttype.length > 0)
     {
         xhr.overrideMimeType(contenttype);
         xhr.setRequestHeader("Content-Type", contenttype);
@@ -23,7 +20,8 @@ function asyncRequest(method, url, body, onloadCallback, size = 0, contenttype =
 
     //alert("Size:"+size);
 
-    if(size > 0) {
+    if (size > 0)
+    {
         xhr.setRequestHeader("Content-Length", size.toString());
         xhr.setRequestHeader("Real-Shit-Size", size.toString());
     }
@@ -42,11 +40,11 @@ function reloadContainerByResponse(dataResponse, containerId)
 {
     var newContainer = dataResponse;
     //Baum überschreiben.
-    if(newContainer.length > 0)
+    if (newContainer.length > 0)
     {
         var htmlIndx = newContainer.indexOf("<html");
 
-        if(htmlIndx >= 0)
+        if (htmlIndx >= 0)
         {
             while (i < newContainer.length && newContainer[htmlIndx] != '>')
             {
@@ -55,34 +53,44 @@ function reloadContainerByResponse(dataResponse, containerId)
             newContainer = newContainer.substring(htmlIndx, newContainer.indexOf("</html>"));
         }
         var index = newContainer.indexOf("id=\"" + containerId + "\"");
-        if(index > 0 || containerId == "html")
+        if (index > 0 || containerId == "html")
         {
-            if (containerId == "html") {
+            if (containerId == "html")
+            {
                 var container = document.getElementsByTagName("html")[0];
                 container.innerHTML = newContainer;
-            } else {
+            }
+            else
+            {
                 var dom = parseDOMelementExtractById(dataResponse, containerId);
-                if (dom) {
+                if (dom)
+                {
                     var container = document.getElementById(containerId);
                     container.innerHTML = dom.innerHTML;
-                } else {
+                }
+                else
+                {
                     var tagName = "";
                     var i = index;
-                    while (i > 0 && newContainer[i] != '<') {
+                    while (i > 0 && newContainer[i] != '<')
+                    {
                         i--;
                     }
                     var a = i;
-                    while (i < newContainer.length && newContainer[i] != ' ' && newContainer[i] != '>') {
+                    while (i < newContainer.length && newContainer[i] != ' ' && newContainer[i] != '>')
+                    {
                         i++;
                     }
                     tagName = newContainer.substring(a + 1, i);
-                    while (i < newContainer.length && newContainer[i] != '>') {
+                    while (i < newContainer.length && newContainer[i] != '>')
+                    {
                         i++;
                     }
                     a = i;
                     i = newContainer.indexOf("</" + tagName);
                     //Letztes
-                    while (newContainer.indexOf("</" + tagName, i + 1) > i) {
+                    while (newContainer.indexOf("</" + tagName, i + 1) > i)
+                    {
                         i = newContainer.indexOf("</" + tagName, i + 1);
                     }
                     newContainer = newContainer.substring(a + 1, i - 1);
@@ -91,7 +99,7 @@ function reloadContainerByResponse(dataResponse, containerId)
 
                 }
             }
-            
+
         }
         else
         {
@@ -101,7 +109,7 @@ function reloadContainerByResponse(dataResponse, containerId)
     else
     {
         var updatedUrl = document.location.href.toString().replaceAll("#", "");
-        if(updatedUrl.includes("?"))
+        if (updatedUrl.includes("?"))
         {
             updatedUrl = updatedUrl.substring(0, updatedUrl.indexOf("?"));
         }
@@ -113,17 +121,21 @@ function reloadContainerByResponse(dataResponse, containerId)
  * Ein Button mit type button verhält sich wie eine button submit, allerdings über asynkronen Request.
  * Asyncron
  */
-function executeFormByMemberEvent( that = null, callBackEvent = null)
+function executeFormByMemberEvent(that = null, callBackEvent = null)
 {
-    if(that) {
+    if (that)
+    {
         //Button finden
-        if (that && that.localName != "button") {
-            while (that && that.localName != "button" && that.localName != "html") {
+        if (that && that.localName != "button")
+        {
+            while (that && that.localName != "button" && that.localName != "html")
+            {
                 that = that.parentElement;
             }
         }
 
-        if (that == null || that.localName != "button") {
+        if (that == null || that.localName != "button")
+        {
             console.log("Error executeFormByMemberEvent: Button not found!");
             return;
         }
@@ -134,20 +146,20 @@ function executeFormByMemberEvent( that = null, callBackEvent = null)
         {
             parent = parent.parentElement;
         }
-        if(parent && parent.localName == "form")
+        if (parent && parent.localName == "form")
         {
             var method = parent.method;
             var url = parent.action;
             //Trennen von relativen Teil-URLs
             var relative = url.indexOf(":");
-            if(relative >= 0)
+            if (relative >= 0)
             {
                 var parts = url.split(":");
-                parts = parts[parts.length-1].split("/");
+                parts = parts[parts.length - 1].split("/");
                 url = "/";
-                for(var i = 1; i < parts.length; i++)
+                for (var i = 1; i < parts.length; i++)
                 {
-                    if(parts[i] != '.')
+                    if (parts[i] != '.')
                     {
                         url += parts[i];
                         if (i + 1 < parts.length)
@@ -157,39 +169,52 @@ function executeFormByMemberEvent( that = null, callBackEvent = null)
                     }
                 }
             }
-            if(url.startsWith("//"))
+            if (url.startsWith("//"))
             {
                 url = url.substr(1);
             }
-            
+
 
             var inputs = parent.getElementsByTagName("input");
             var texts = parent.getElementsByTagName("textarea");
+            var selects = parent.getElementsByTagName("select");
             var bodyParams = "";
             var attached = 0;
             //Inputs an body anhängen
-            for(var i = 0; i < inputs.length; i++)
+            for (var i = 0; i < inputs.length; i++)
             {
                 var inp = inputs[i];
-                if (i > 0) {
+                if (i > 0)
+                {
                     bodyParams += "&";
-                    attached++;
                 }
                 bodyParams += decodeURI(inp.name) + "=" + decodeURI(inp.value);
+                attached++;
             }
-            for(var i = 0; i < texts.length; i++)
+            for (var i = 0; i < selects.length; i++)
+            {
+                var inp = selects[i];
+                if (i > 0 || attached > 0)
+                {
+                    bodyParams += "&";
+                }
+                bodyParams += decodeURI(inp.name) + "=" + decodeURI(inp.value);
+                attached++;
+            }
+            for (var i = 0; i < texts.length; i++)
             {
                 var inp = texts[i];
-                if(inp.name && inp.name.length > 0)
+                if (inp.name && inp.name.length > 0)
                 {
-                    if (attached > 0) {
+                    if (i > 0 || attached > 0)
+                    {
                         bodyParams += "&";
-                        attached++;
                     }
                     bodyParams += decodeURI(inp.name) + "=" + decodeURI(inp.innerText);
+                    attached++;
                 }
             }
-            if(callBackEvent)
+            if (callBackEvent)
             {
                 showLoadingProgressSpinner(5);
                 asyncRequest(method, url, bodyParams, callBackEvent);
@@ -218,7 +243,7 @@ function executeFormByMemberEvent( that = null, callBackEvent = null)
  */
 function askToExecute(func, confirmMsg)
 {
-    if(confirm(confirmMsg))
+    if (confirm(confirmMsg))
     {
         func();
     }
@@ -232,14 +257,15 @@ function sleepMilli(milliSeconds)
 {
     var start = Date.now();
     var op = "";
-    while ((Date.now()-start) < milliSeconds)
+    while ((Date.now() - start) < milliSeconds)
     {
         op = "abcde";
     }
 }
 
 /** Kopiert die Daten eines Eintrages der Auswahlliste in das Neue-Zeile-Formular. */
-function copyValue_SelectionToEntry(Selection, idEntry) {
+function copyValue_SelectionToEntry(Selection, idEntry)
+{
     var e = document.getElementById(idEntry);
     e.setAttribute("value", Selection.value);
 
@@ -252,45 +278,47 @@ function parseDOMelementExtractById(htmlString, elementId)
     var xml = parser.parseFromString(htmlString.toString(), 'text/html');
     return xml.getElementById(elementId);
 }
+
 /** Sorgt dafür, dass alle <img>'s im Element mit der Id neu geladen werden*/
 function reloadImagesInElement(elementId)
 {
     var element = document.getElementById(elementId);
-    if(element) {
+    if (element)
+    {
         var imgs = element.getElementsByTagName('img');
         var customParam = "timestampforreloadimg";
-        for(var i=0; i < imgs.length; i++)
+        for (var i = 0; i < imgs.length; i++)
         {
             var imgsrc = imgs[i].src;
             var timestampIndex = imgsrc.indexOf(customParam);
-            if(timestampIndex >= 0)
+            if (timestampIndex >= 0)
             {
                 //Gefunden
                 //Herausschneiden des vorherigen customParam
                 var d = timestampIndex;
                 while (imgsrc[d] != '&')
                 {
-                    if(d+1 == imgsrc.length)
+                    if (d + 1 == imgsrc.length)
                     {
                         break;
                     }
                     d++;
                 }
-                if(d+1 == imgsrc.length)
+                if (d + 1 == imgsrc.length)
                 {
                     //Das timestampIndex-1 folgt aus &timestampIndex oder ?timestampIndex
-                    imgsrc = imgsrc.substr(0, timestampIndex-1);
+                    imgsrc = imgsrc.substr(0, timestampIndex - 1);
                 }
                 else
                 {
-                    var partA = imgsrc.substr(0, timestampIndex-1);
+                    var partA = imgsrc.substr(0, timestampIndex - 1);
                     var partB = imgsrc.substr(d);
                     imgsrc = partA + partB;
                 }
             }
 
             //Die src wird verändert, damit sich das Bild neu geladen wird.
-            if(imgsrc.indexOf('?') >= 0)
+            if (imgsrc.indexOf('?') >= 0)
             {
                 imgsrc += '&' + customParam + '=' + new Date().getTime();
             }
@@ -307,10 +335,10 @@ function removeThisModal(element)
 {
     var p = element.parentNode;
     var l = 0;
-    while(!p.classList.contains("modal"))
+    while (!p.classList.contains("modal"))
     {
         p = p.parentNode;
-        if(p == null || l > 20)
+        if (p == null || l > 20)
         {
             return;
         }
@@ -325,7 +353,7 @@ function pushNotification(title, text, duration = 3000)
         "  <div class=\"toast\" style=\"position: absolute; top: 0; right: 0;\">\n" +
         "    <div class=\"toast-header\">\n" +
         "      <img src=\"...\" class=\"rounded mr-2\" alt=\"...\">\n" +
-        "      <strong class=\"mr-auto\">"+title+"</strong>\n" +
+        "      <strong class=\"mr-auto\">" + title + "</strong>\n" +
         "      <button type=\"button\" class=\"ml-2 mb-1 close\" data-dismiss=\"toast\" aria-label=\"Close\">\n" +
         "        <span aria-hidden=\"true\">&times;</span>\n" +
         "      </button>\n" +
@@ -340,7 +368,8 @@ function pushNotification(title, text, duration = 3000)
     notification.innerHTML = code;
     notification.id = "pushNotification";
     document.getElementsByTagName("body")[0].insertBefore(notification, document.getElementsByTagName("body")[0].children[0]);
-    window.setTimeout(function rem(){
+    window.setTimeout(function rem()
+    {
         document.getElementById("pushNotification").remove();
     }, duration);
 }
